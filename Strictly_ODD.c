e95c14267cb4344c1ca113c4ba4cee12cf31c6bb
sirichakra <147473243+sirichakra@users.noreply.github.com>
2024-03-14 20:02:50 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     int cnt=0,sum=0;     for(i=0;i<n;i++){         if(a[i]%2!=0){             sum++;             if(i%2!=0)             cnt++;         }     }     if(cnt==sum)     printf("True");     else     printf("False"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int cnt=0,sum=0;
+    for(i=0;i<n;i++){
+        if(a[i]%2!=0){
+            sum++;
+            if(i%2!=0)
+            cnt++;
+        }
+    }
+    if(cnt==sum)
+    printf("True");
+    else
+    printf("False");
+}
